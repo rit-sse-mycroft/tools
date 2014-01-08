effb17d7e10ea93035887285d177c0d252ca035d
Ethan H. Jurman <ehj2229@rit.edu>
2014-01-08 14:06:00 -0500

    modified the mock app, checking status and reading type

--- mock_app/mock_app.js
+++ mock_app/mock_app.js
@@ -1,13 +1,38 @@
 var net = require('net');
 var manifest = require('./app.json');
-var message = require('./message.json');
+
 var client = net.connect({port: 1847}, function() {
   console.log('client connected');
   client.write(manifest);
 });
 
-client.on('data', function(data) {
-  client.write(message);
+client.on('data', function (data) {
+  dataMatch = /response (.*)/.exec(data);
+  if (dataMatch.length != 2) {
+    message = 'invalid json response';
+  } else {
+    data = JSON.parse(dataMatch[1]);
+    
+    if (data.status == 'STATUS_GOOD') {
+      message = {
+        timestamp: new Date().toISOString(),
+        token: data.token,
+        content: 'pickle unicorns'
+      }
+      var messageBoard = net.connect({ port: data.port }, function () {
+        console.log('connected to message board');
+        console.log(message);
+        messageBoard.write('message ' + JSON.stringify(data));
+      });
+    } else if (data.status == 'MISSING_DEPENDENCIES') {
+      message = 'missing dependencies';
+    } else if (data.status == 'INVALID_CONFIG') {
+      message = 'invalid config';
+    } else {
+      message = 'something went wrong';
+    }
+  }
+  console.log(message);
 });
 
 client.on('end', function() {
