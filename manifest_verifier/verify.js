b05f7a891ba9805ea7e14918d0ef5e326fc0ea9e
weswigham <wwigham@gmail.com>
2014-01-07 23:20:47 -0500

    Clarified some rules, added a scope
    made the semantic version verifier more valid

--- manifest_verifier/verify.js
+++ manifest_verifier/verify.js
@@ -7,33 +7,52 @@ process.stdin.on('data', function(chunk) {
 });
 
 function isSemantic(str) {
-  if (!(str instanceof String)) {
-    process.stdout.write("#ERROR '"+str+"' is not a string instance and cannot be parsed into a semantic version.\n");
+  if (typeof(str) === "number" && !isNaN(Number(str))) {
+    if (Number(str) >= 0) {
+      return true; //Must be something like 4.333333, which is valid, if bad.
+    }
+    return false; //-4.3333, on the other hand, is wrong.
+  }
+  
+  if (typeof(str) !== "string") {
+    process.stdout.write("#ERROR '" + str + "' is not a string instance and cannot be parsed into a semantic version.\n");
     return false;
   }
   
   var triple = str.split('.');
   if (triple.length > 3) {
-    process.stdout.write("#ERROR '"+str+"' has more than three .'s and can't be a semantic version.\n");
+    process.stdout.write("#ERROR '" + str + "' has more than two .'s and can't be a semantic version.\n");
     return false;
   }
+
   
-  if ((triple[1] && isNan(Number(triple[1]))) || (triple[2] && isNaN(Number(triple[2])))) {
-    process.stdout.write("#ERROR components of version string '"+str+"' were not numeric.\n");
+  if ((triple[1] && isNaN(Number(triple[1]))) || (triple[2] && isNaN(Number(triple[2])))) {
+    process.stdout.write("#ERROR components of version string '" + str + "' were not numeric.\n");
     return false;
   } 
   
-  var prefixes = ["~", "<", "<=", "="];
-  for (v in prefixes) {
-    if (triple[0].indexOf(v) === 0) {
+  if ( (triple[1] && triple[1].trim() === "") || (triple[2] && triple[2].trim() === "")) {
+    process.stdout.write("#ERROR components of version string '" + str + "' did not exist.\n");
+    return false;
+  }
+  
+  triple[0] = triple[0].trim();
+  var prefixes = ["~", "<=", "<", "="];
+  for (k in prefixes) {
+    if (triple[0].indexOf(prefixes[k]) === 0) {
       //this prefix is present
-      triple[0] = triple[0].trim(v);
+      triple[0] = triple[0].substring(prefixes[k].length);
       break;
     }
   }
   
-  if (triple[0] && isNan(Number(triple[0]))) {
-    process.stdout.write("#ERROR first component of version string '"+str+"' was not numeric.\n");
+  if (triple[0].trim() === "") {
+    process.stdout.write("#ERROR first component of version string '" + str + "' did not exist.\n");
+    return false;
+  }
+  
+  if (triple[0] && isNaN(Number(triple[0]))) {
+    process.stdout.write("#ERROR first component of version string '" + str + "' was not numeric.\n");
     return false;
   }
   
@@ -48,19 +67,16 @@ var template = {
   "displayname":false, //optional
   "type": true,
   "API": function(val) {
-    return (val===floor(val)); //is an integer
+    return (!isNaN(Math.floor(val)) && val==Math.floor(val)); //is an integer, yes I want == type coercion here.
   },
-  "description": function(val) {
-    //warn
-    process.stdout.write("#WARN No description field given.\n");
-    return true;
-  },
+  "description": false,
   "dependencies": function(val) {
     for (k in val) {
       if (!isSemantic(val[k])) {
         return false;
       }
     }
+    return true;
   }
 }
 
@@ -69,35 +85,42 @@ function isFunction(functionToCheck) {
   return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 }
 
-var errors = [];
+function verifyManifestJSON(inp) {
 
-function verifyManifestJSON(obj, rules) {
-  for (k in rules) {
-    if (k in obj) {
+  var errors = [];
+
+  function verifyJSON(obj, rules) {
+    for (k in rules) {
       if (isFunction(rules[k])) {
         if (!rules[k](obj[k])) {
           //failed rules check  
           errors.push("not ok "+errors.length+" Failed rule '"+k+"'.")
         }
+        //otherwise pass, do nothing
       } else {
-        if (rules[k] instanceof Object) {
-          //recur
-          verifyManifestJSON(obj[k], rules[k]);
+        if (k in obj) {
+          if (rules[k] instanceof Object) { //see if we need to recur
+            //recur
+            verifyJSON(obj[k], rules[k]);
+          }
+        } else { //missing a key we expected
+          if (rules[k] !== false) { //was it optional?
+            //required key missing
+            errors.push("not ok " + errors.length + " Required key '" + k + "' missing.")
+          } else { //it was optional - TODO: write a WARNING comment instead of ok?
+            errors.push("ok " + errors.length + " Recommended key '" + k + "' missing.")
+          }
         }
-      }
-    } else {
-      if (rules[k]!==false) {
-        //required key missing
-        errors.push("not ok "+errors.length+" Required key '"+k+"' missing.")
-      } else {
-        errors.push("ok "+errors.length+" Recommended key '"+k+"' missing.")
       }
     }
   }
+  
+  verifyJSON(inp, template);
+  return errors;
 }
 
 process.stdin.on('end', function() {
-  verifyManifestJSON(JSON.parse(jsondata), template);
+  var errors = verifyManifestJSON(JSON.parse(jsondata));
   var bad = 1;
   for (k in errors) {
     bad = 0;
