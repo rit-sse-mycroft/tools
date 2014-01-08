1752e825bb4bff10f5fe1836631297dcfe5a7c3e
AJ Deck <ajd4223@rit.edu>
2014-01-08 12:52:09 -0500

    Added mock_app.js

---
+++ mock_app/mock_app.js
@@ -0,0 +1,17 @@
+
+var net = require('net');
+var manifest = require('./app.json')
+var message = require('./message.json')
+var client = net.connect({port: 1847},
+	fuction() {
+		console.log('client connected');
+		client.write(manifest);
+	});
+
+client.on('data', function() {
+	client.write(message);
+});
+
+client.on('end', function() {
+	console.log('client disconnected');
+})
