51b833675d500a37757eb1f80c19685390b9b5af
kristenmills <kristen@kristen-mills.com>
2014-01-08 13:01:42 -0500

    Fixed missing semicolons

--- mock_app/mock_app.js
+++ mock_app/mock_app.js
@@ -1,12 +1,12 @@
 var net = require('net');
-var manifest = require('./app.json')
-var message = require('./message.json')
+var manifest = require('./app.json');
+var message = require('./message.json');
 var client = net.connect({port: 1847}, function() {
   console.log('client connected');
   client.write(manifest);
 });
 
-client.on('data', function() {
+client.on('data', function(data) {
   client.write(message);
 });
 
