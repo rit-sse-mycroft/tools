171e04e5a2bc96c810a262c645c142f16c623d5f
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2014-01-09 10:40:17 -0500

    Fixed js for mockapp

--- mock_app/mock_app.js
+++ mock_app/mock_app.js
@@ -1,15 +1,18 @@
-var net = require('net');
-var app = require('./app.js');
+var net = require('net'),
+  app = require('./app.js'),
+  client = app.connectToMycroft();
 
-var client = app.connectToMycroft();
 app.sendManifest(client, './app.json');
 
 client.on('data', function (data) {
-  dataMatch = /MANIFEST_(OK||FAIL) (.*)/.exec(data);
+  var dataMatch = /MANIFEST_(OK||FAIL) (.*)/.exec(data),
+    message = '',
+    data;
+
   if (dataMatch.length != 3) {
     message = 'invalid json response';
   } else {
-    data = JSON.parse(dataMatch[2]);
+    data = JSON.parse(dataMatch[2]);  
     console.log('Response type: MANIFEST_' + dataMatch[1]);
     console.log('Response recieved:');
     console.log(data);
@@ -17,14 +20,11 @@ client.on('data', function (data) {
     if (dataMatch[1] === 'OK') {
       var messageBoard = net.connect({ port: data.dataPort }, function (err) {
         console.log('Connecting to Message Board...');
-
         if (err) {
           console.error('Error connecting to Message Board');
         }
       });
-
       app.sendMessage(messageBoard, data.instanceId, 'pickle unicorns');
-
     } else if (dataMatch[1] === 'FAIL') {
       message = 'Manifest validation failed';
     } else {
@@ -33,6 +33,7 @@ client.on('data', function (data) {
   }
   console.log(message);
 });
+
 
 client.on('end', function() {
   console.log('client disconnected');
