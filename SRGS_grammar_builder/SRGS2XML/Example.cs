77552dde4201e231db08e19735d24e8d37c2f16f
weswigham <wwigham@gmail.com>
2014-01-10 16:09:50 -0500

    Command line C# grammar to XML grammar converter

---
+++ SRGS_grammar_builder/SRGS2XML/Example.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition.SrgsGrammar;
+
+namespace SRGS2XML
+{
+    class Example : ISRGSSpec
+    {
+        private SrgsDocument doc;
+
+        public Example()
+        {
+            doc = new SrgsDocument();
+
+            SrgsRule whatis = new SrgsRule("whatis");
+
+            SrgsItem question = new SrgsItem("what is");
+            SrgsOneOf closers = new SrgsOneOf("life", "the time", "love", "happiness", "two plus two");
+
+            whatis.Add(question);
+            whatis.Add(closers);
+            whatis.Scope = SrgsRuleScope.Public;
+
+            doc.Rules.Add(whatis);
+            doc.Root = whatis;
+        }
+
+        public SrgsDocument SRGS()
+        {
+            return doc;
+        }
+    }
+}
