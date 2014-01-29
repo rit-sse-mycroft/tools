d81810de75d95da85ff13cc7d8d365de0491a5a8
kristenmills <kristen@kristen-mills.com>
2014-01-29 10:50:32 -0500

    Added the mock microphone app

---
+++ mock_microphone/app.js
@@ -0,0 +1,194 @@
+var tls = require('tls');
+var net = require('net');
+var uuid = require('uuid');
+var fs = require('fs');
+var MYCROFT_PORT = 1847;
+
+var _unconsumed = '';
+
+// Parses a received message and returns an array of commands as
+// an Object containing type:String and data:Object.
+function parseMessage(msg) {
+  // Add the message to unconsumed.
+  _unconsumed += msg.toString().trim();
+  // Create an array for the newly parsed commands.
+  var parsedCommands = [];
+  
+  while (_unconsumed != '') {
+    // Get the message-length to read.
+    var verbStart = _unconsumed.indexOf('\n');
+    var msgLen = parseInt(_unconsumed.substr(0, verbStart));
+    // Cut off the message length header from unconsumed.
+    _unconsumed = _unconsumed.substr(verbStart+1);
+    // Figure out how many bytes we have left to consume.
+    var bytesLeft = Buffer.byteLength(_unconsumed, 'utf8');
+    // Do not process anything if we do not have enough bytes.
+    if (bytesLeft < msgLen) {
+      break;
+    }
+    // Isolate the message we are actually handling.
+    var unconsumedBuffer = new Buffer(_unconsumed);
+    msg = unconsumedBuffer.slice(0, msgLen).toString();
+    // Store remaining stuff in unconsumed.
+    _unconsumed = unconsumedBuffer.slice(msgLen).toString();
+    // Go process this single message.
+    console.log('Got message:');
+    console.log(msg);
+    var type = '';
+    var data = {};
+    var index = msg.indexOf(' {');
+    if (index >= 0) { // If a body was supplied
+      type = msg.substr(0, index);
+      try {
+        var toParse = msg.substr(index+1);
+        data = JSON.parse(toParse);
+      }
+      catch(err) {
+        console.error('Malformed message 01');
+        sendMessage(cli, "MSG_MALFORMED \n" + err);
+        return;
+      }
+    } else { // No body was supplied
+      type = msg;
+    }
+    
+    parsedCommands.push({type: type, data: data});
+  }
+  return parsedCommands;
+}
+
+// If using TLS, appName is assumed to be the name of the keys.
+function connectToMycroft(appName) {
+  var client = null;
+  if (process.argv.length === 3 && process.argv[2] === '--no-tls') {
+    console.log("Not using TLS");
+    client = net.connect({port: 1847}, function(err) {
+      if (err) {
+        console.error('There was an error establishing connection');
+      }
+    });
+  } else {
+    console.log("Using TLS");
+    var connectOptions = {
+      key: fs.readFileSync(appName + '.key'),
+      cert: fs.readFileSync(appName + '.crt'),
+      ca: [ fs.readFileSync('ca.crt') ],
+      rejectUnauthorized: false,
+      port: MYCROFT_PORT
+    };
+    client = tls.connect(connectOptions, function(err) {
+      if (err) {
+        console.error('There was an error in establishing TLS connection');
+      }
+    });
+  }
+  console.log('Connected to Mycroft');
+  return client;
+}
+
+//Given the path to a JSON manifest, converts that manifest to a string,
+//and precedes it with the type MANIFEST
+function sendManifest(client, path) {
+  try {
+    var manifest = require(path);
+  }
+  catch(err) {
+    console.error('Invalid file path');
+  }
+  console.log('Sending Manifest');
+  sendMessage(client, 'APP_MANIFEST', manifest)
+}
+
+function up(client) {
+  console.log('Sending App Up');
+  sendMessage(client, 'APP_UP');
+}
+
+function down(client) {
+  console.log('Sending App Down');
+  sendMessage(client, 'APP_DOWN');
+}
+
+function query(client, capability, action, data, instanceId, priority) {
+  queryMessage = {
+    id: uuid.v4(),
+    capability: capability,
+    action: action,
+    data: data,
+    priority: priority,
+
+  };
+  if (typeof(instanceId) != 'undefined') queryMessage.instanceId = instanceId;
+
+  sendMessage(client, 'MSG_QUERY', queryMessage);
+}
+
+function querySuccess(client, id, ret ) {
+  var querySuccessMessage = {
+    id: id,
+    ret: ret
+  };
+
+  sendMessage(client, 'MSG_QUERY_SUCCESS', querySuccessMessage);
+}
+
+function queryFail(client, id, message) {
+  var queryFailMessage = {
+    id: id,
+    message: message
+  };
+
+  sendMessage(client, 'MSG_QUERY_FAIL', queryFailMessage);
+}
+
+//Sends a message to the Mycroft global message board.
+function broadcast(client, content) {
+  message = {
+    id: uuid.v4(),
+    content: content
+  };
+  sendMessage(client, 'MSG_BROADCAST', message);
+}
+
+// Checks if the manifest was validated and returns dependencies
+function manifestCheck(parsed) {
+  if (parsed.type === 'APP_MANIFEST_OK' || parsed.type === 'APP_MANIFEST_FAIL') {
+    console.log('Response type: ' +  parsed.type);
+    console.log('Response recieved: ' + JSON.stringify(parsed.data));
+
+    if (parsed.type === 'APP_MANIFEST_OK') {
+      console.log('Manifest Validated');
+      return parsed.data.dependencies;
+    } else {
+      throw 'Invalid application manifest';
+    }
+  }
+}
+
+//Sends a message of specified type. Adds byte length before message.
+//Does not need to specify a message object. (e.g. APP_UP and APP_DOWN)
+function sendMessage(client, type, message) {
+  if (typeof(message) === 'undefined') {
+    message = '';
+  } else {
+    message = JSON.stringify(message);
+  }
+  var body = (type + ' ' + message).trim();
+  var length = Buffer.byteLength(body, 'utf8');
+  console.log('Sending Message');
+  console.log(length);
+  console.log(body);
+  client.write(length + '\n' + body);
+
+}
+
+exports.parseMessage = parseMessage
+exports.connectToMycroft = connectToMycroft;
+exports.sendManifest = sendManifest;
+exports.up = up;
+exports.down = down;
+exports.query = query;
+exports.querySuccess = querySuccess;
+exports.queryFail = queryFail;
+exports.broadcast = broadcast;
+exports.manifestCheck = manifestCheck;
---
+++ mock_microphone/microphone.js
@@ -0,0 +1,55 @@
+var APP_NAME = 'mock-microphone';
+
+var prompt = require('prompt');
+var app = require('./app.js');
+var client = app.connectToMycroft(APP_NAME);
+
+app.sendManifest(client, './app.json');
+
+var verified = false; //Set to true when APP_MANIFEST_OKAY received
+var takingInput = false;
+
+client.on('data', function(msg) {
+  parsed = app.parseMessage(msg);
+  for (var i = 0; i < parsed.length; i++) {
+    handleMessage(parsed[i]);
+  }
+});
+
+client.on('end', function() {
+  console.log('Client disconnected.');
+});
+
+// Handle a single command.
+// parsed is a parsed command (as JSON) with type:String and data:Object.
+function handleMessage(parsed) {
+  // Check the type of this message.
+  if (parsed.type === 'APP_MANIFEST_OK'/* || parsed.type === 'APP_MANIFEST_FAIL'*/) {
+    var dependencies = app.manifestCheck(parsed.data);
+    verified = true;
+  } else if (parsed.type === 'APP_DEPENDENCY') {
+    if(!takingInput) {
+      promptMessage();
+      takingInput = true;
+    }
+    app.up(client);
+  } else {
+    console.log('Message Receieved');
+    console.log(' - Type: ' + parsed.type);
+    console.log(' - Message:' + JSON.stringify(parsed.data));
+  }
+}
+
+function promptMessage() {
+  prompt.start();
+  prompt.get(['text'], function(err, result) {
+    if (!err) {
+      var message = {
+        spoken_text: result.text
+      };
+      // Broadcast the input.
+      app.broadcast(client, message);
+    }
+    promptMessage();
+  });
+}
